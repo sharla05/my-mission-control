2379362efe744f9d43198d4a886e08de50590c00
Lalit Sharma <lalitsharma.email@gmail.com>
2025-08-13 11:34:35 -0400

    pyright

--- src/my_mission_control/alerter/log_file_processor_v2.py
+++ src/my_mission_control/alerter/log_file_processor_v2.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from structlog.stdlib import get_logger
 
--- src/my_mission_control/utils/__init__.py
+++ src/my_mission_control/utils/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional
 
 import structlog
 import tomli as toml
